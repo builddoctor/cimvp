25f12bacba71a9fbb1187024770586a421887888
simpsonjulian <simpsonjulian@gmail.com>
2011-08-05 15:19:09 +0100

    more tests

--- spec/my_great_spec.rb
+++ spec/my_great_spec.rb
@@ -1,5 +1,16 @@
-describe "my code" do 
+def zombie_say
+  "Must. Eat. Brains"
+end
+describe "my great code" do 
   it "should be able to evaluate true" do 
 	  true.should == true
+  end
+
+  it "should not be in Orwell's 1984" do
+    (2 + 2).should == 4
+  end
+
+  it "should know what zombies say" do 
+    zombie_say.should match /brains/i
   end
 end
